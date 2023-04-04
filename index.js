3d909c5f70b943fd36f6d308d3720796ba59a0e2
abhishek781 <20abhishek16@gmail.com>
2023-04-04 21:05:27 +0530

    second commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 
 const students = [
-  { ID: 1, name: 'Alice', age: 21, grade: 'A', degree: 'Btech', email: 'alice@example.com' },
+  //ID: 1, name: 'Alice', age: 21, grade: 'A', degree: 'Btech', email: 'alice@example.com' },
   // { ID: 2, name: 'Bob', age: 22, grade: 'B', degree: 'MBA', email: 'bob@example.com' },
   // { ID: 3, name: 'Charlie', age: 20, grade: 'C', degree: 'Arts', email: 'charlie@example.com' }
 ];
